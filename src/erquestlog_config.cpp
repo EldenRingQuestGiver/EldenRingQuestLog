7051e345ae336cdbb0cefd4f3b0cdcc2ee4fa5cc
EldenRingQuestGiver <giverthequest@gmail.com>
2024-09-07 22:49:23 +0200

    github is dumb

---
+++ src/erquestlog_config.cpp
@@ -0,0 +1,24 @@
+#include "erquestlog_config.hpp"
+
+#include <mini/ini.h>
+#include <spdlog/spdlog.h>
+#include <string.h>
+
+extern std::string erquestlog::config::lang = "none";
+
+void erquestlog::load_config(const std::filesystem::path &ini_path)
+{
+    spdlog::info("Loading config from {}", ini_path.string());
+
+    mINI::INIFile file(ini_path.string());
+    mINI::INIStructure ini;
+    if (file.read(ini) && ini.has("erquestlog"))
+    {
+        auto &config = ini["erquestlog"];
+
+        if (config.has("lang"))
+            config::lang = config["lang"];
+
+        spdlog::info("Overriding language = {}", config::lang);
+    }
+}
---
+++ src/erquestlog_messages.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <filesystem>
+
+#include "from/messages.hpp"
+
+namespace erquestlog
+{
+
+void setup_messages(std::filesystem::path folder);
+const std::wstring_view get_message(from::msgbnd, int);
+
+}
